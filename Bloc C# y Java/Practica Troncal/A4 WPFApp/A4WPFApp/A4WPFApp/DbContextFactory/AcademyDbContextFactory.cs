f0d9be8d81010931aa937cc95edc880829bdb8b7
marcvil <marcvilabascara@gmail.com>
2020-03-31 11:06:33 +0200

    Factory y builder realizados

--- Bloc C# y Java/Practica Troncal/A4 WPFApp/A4WPFApp/A4WPFApp/DbContextFactory/AcademyDbContextFactory.cs
+++ Bloc C# y Java/Practica Troncal/A4 WPFApp/A4WPFApp/A4WPFApp/DbContextFactory/AcademyDbContextFactory.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using A4_DAL;
+using System.IO;
+
+
+
 
 namespace A4WPFApp.DbContextFactory
 {
-    class AcademyDbContextFactory
+    public class AcademyDbContextFactory : IDesignTimeDbContextFactory<AcademyDbContext>
     {
+        public AcademyDbContext CreateDbContext(string[] args)
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("Appsettings.json", optional: false, reloadOnChange: true);
 
+
+            var configuration = builder.Build();
+            var dbConnection = configuration.GetConnectionString("AcademyDbConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<AcademyDbContext>();
+            optionsBuilder.UseSqlite(dbConnection, x => x.MigrationsAssembly("A4WPFApp"));
+
+            return new AcademyDbContext(optionsBuilder.Options);
+        }
     }
 }
